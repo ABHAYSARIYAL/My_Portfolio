8bcdefd849eea0d12f27868fba159136e5991a9c
ABHAYSARIYAL <abhaysariyal143@gmail.com>
2025-07-20 16:31:45 +0530

    new commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -44,7 +44,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1gAJZZN_7bxtjdRVmTdnwBG5IyBt-tQ8m/view?usp=sharing"
+            href="https://drive.google.com/file/d/1jtl6xjdUxF__Br7v8rFfMyI9s_Cp-BgG/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
